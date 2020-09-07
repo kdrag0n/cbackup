194e8c763995784cf4d3e86d7626dc77686e0f1e
Danny Lin <danny@kdrag0n.dev>
2020-09-06 21:37:22 -0700

    Add detailed header with license

--- cbackup.sh
+++ cbackup.sh
@@ -1,9 +1,36 @@
 #!/usr/bin/env bash
 
+# cbackup: Simple full app + data + metadata backup/restore script for Android
+#
+# Required Termux packages: tsu tar sed zstd pv openssl-tool
+#
+# App data backups are tarballs compressed with Zstandard and encrypted with
+# AES-256-CTR.
+#
+# Licensed under the MIT License (MIT)
+# 
+# Copyright (c) 2020 Danny Lin <danny@kdrag0n.dev>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 set -euo pipefail
 shopt -s nullglob
-
-# Required Termux packages: tsu tar sed zstd pv openssl-tool
 
 # Prints an error in bold red
 function err() {
